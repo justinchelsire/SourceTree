9031e9db17709ead56821a6e6f7448c9e8125af4
justinchelsire <48729124+justinchelsire@users.noreply.github.com>
2020-06-03 14:28:26 +0700

    Update main.dart

--- sourcetree/lib/main.dart
+++ sourcetree/lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title:"Xin Chào Tất Cả Các Bạn!!"
       title: 'Xin Cháo Các Bạn!!!',
       theme: ThemeData(
         // This is the theme of your application.
